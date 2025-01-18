92386ab3b5c2f3cd32049b136ae2d5df6bf88947
LuckyChimp <lukas.hartwich00@gmail.com>
2025-01-18 15:39:37 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	// global.css with variables etc. is inherited by all pages of this website
+	import '$lib/styles/global.css';
+
+	let { children } = $props();
+</script>
+
+<div id="page">
+	<nav></nav>
+	{@render children()}
+</div>
+
+<style>
+	#page {
+		width: 100vw;
+		min-height: 100vh;
+		background-image: url('home/background-home-desktop.jpg');
+		background-size: 100% auto;
+		background-repeat: no-repeat;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { PageServerData } from './$types';
+
+	let { data }: { data: PageServerData } = $props();
+</script>
+
+<main>
+	<h1>Space</h1>
+</main>
